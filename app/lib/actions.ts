77cb17b55acd70b8562912e835bb9623ac521a6d
udutvic <udutvic@gmail.com>
2024-11-28 17:38:26 +0100

    commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,9 +54,10 @@ export async function createInvoice(prevState: State, formData: FormData) {
           INSERT INTO invoices (customer_id, amount, status, date)
           VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
-      } catch (error) {
+    } catch (error) {
+      console.error('Database error:', error);
         return {
-          message: 'Database Error: Failed to Create Invoice.',
+          message: 'Database Error: Failed to Create Invoice.'
         };
       }
     
@@ -87,7 +88,8 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
             WHERE id = ${id}
           `;
-      } catch (error) {
+    } catch (error) {
+      console.error('Database error:', error);
         return { message: 'Database Error: Failed to Update Invoice.' };
       }
    
@@ -101,7 +103,8 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         expirePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
-      } catch (error) {
+    } catch (error) {
+      console.error('Database error:', error);
         return { message: 'Database Error: Failed to Delete Invoice.' };
       }
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { Console } from 'console';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
