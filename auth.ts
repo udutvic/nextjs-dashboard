71f9d78e7149b9a4dfbe77f669bad23df4585e96
udutvic <udutvic@gmail.com>
2024-11-28 17:49:51 +0100

    commit

--- auth.ts
+++ auth.ts
@@ -16,7 +16,7 @@ async function getUser(email: string): Promise<User | undefined> {
     }
 }
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [Credentials({
         async authorize(credentials) {
