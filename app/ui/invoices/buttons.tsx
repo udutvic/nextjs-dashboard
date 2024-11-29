17d4514a5459865f349cfeb6178bc934908a9d45
udutvic <udutvic@gmail.com>
2024-11-29 15:23:56 +0100

    commit

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
+import { useTransition } from 'react';
+
 
 export function CreateInvoice() {
   return (
@@ -25,11 +29,21 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: { id: string }) {    
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const [isPending, startTransition] = useTransition();
+
+  const handleDelete = () => {
+    const confirmed = window.confirm('Are you sure you want to delete this invoice? This action cannot be undone.');
+    if (confirmed) {
+      startTransition(() => {
+        deleteInvoiceWithId();
+      });
+    }
+  };
   return (
-    <form action={deleteInvoiceWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+    <form >
+      <button type="button" onClick={handleDelete} className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
